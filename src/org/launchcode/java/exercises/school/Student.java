5c67a5e8d30edd5046fc284447e646361d0ace69
Bryan Maxwell Dobberstein <max.dobberstein@gmail.com>
2017-05-05 16:20:41 -0500

    Complete Class 6 studio review

--- src/org/launchcode/java/exercises/school/Student.java
+++ src/org/launchcode/java/exercises/school/Student.java
@@ -86,4 +86,19 @@ public class Student {
     public String toString(){
         return "Name: " + this.name + "ID: " + this.id + "Credits: " + this.credits + "Level: " + this.getGradeLevel() + "GPA: " + this.gpa;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
